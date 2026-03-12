016f1b0a99b6c2b585c494f1f8c491d29a3acc4d
Nguyễn Minh Mẫn <maiphuc455@gmail.com>
2026-03-12 12:41:34 +0700

    test process ci 1

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, DevOps!</h1>'
+    return '<h1>Hello, DevOps! 1</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
